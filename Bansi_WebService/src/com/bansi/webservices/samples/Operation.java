7e8e72012854973ad947bb42fa33cfda0df732a4
zbansi <alonjune@gmail.com>
2019-01-05 09:41:16 +0800

    20190105 am3

---
+++ Bansi_WebService/src/com/bansi/webservices/samples/Operation.java
@@ -0,0 +1,12 @@
+package com.bansi.webservices.samples;
+
+import java.rmi.RemoteException;
+
+/**
+ * <p>Functional interface for storing sample operation into option included in {@code OptionList}.</p>
+ * <p>© 2017 NetSuite Inc. All rights reserved.</p>
+ */
+@FunctionalInterface
+public interface Operation {
+    void performOperation() throws RemoteException;
+}
